c1d070e3947a38ea0ce67209144caa7e40ee60ba
Egalvi <egalvi@mail.ru>
2015-10-21 23:00:33 +0300

    Contact details buttons integration

--- app/src/main/java/kg/gorillagym/gorillagymshop/ContactDetails.java
+++ app/src/main/java/kg/gorillagym/gorillagymshop/ContactDetails.java
@@ -6,10 +6,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import java.io.BufferedReader;
-
+import kg.gorillagym.gorillagymshop.cart.CartHolder;
 import kg.gorillagym.gorillagymshop.navigation.Navigator;
+import ru.egalvi.shop.Cart;
+import ru.egalvi.shop.CartService;
+import ru.egalvi.shop.ClientData;
 
 public class ContactDetails extends AppCompatActivity {
 
@@ -17,6 +20,37 @@ public class ContactDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_details);
+        final CartService cartService = new CartService() {
+            @Override
+            public void checkout(Cart cart, ClientData clientData) {
+
+            }
+        };
+
+        final EditText emailField = (EditText) findViewById(R.id.edit_email);
+        final EditText nameField = (EditText) findViewById(R.id.edit_name);
+        final EditText addressField = (EditText) findViewById(R.id.edit_address);
+        final EditText phoneField = (EditText) findViewById(R.id.edit_phone);
+
+        Button sendButton = (Button) findViewById(R.id.button_send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                cartService.checkout(CartHolder.getCart(), new ClientData() {
+                });
+            }
+        });
+        Button clearButton = (Button) findViewById(R.id.button_clear);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailField.setText("");
+                nameField.setText("");
+                addressField.setText("");
+                phoneField.setText("");
+            }
+        });
     }
 
     @Override
