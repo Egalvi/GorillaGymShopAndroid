ad85ab09a04d56c88cc848f2cef802e35088db16
Egalvi <egalvi@mail.ru>
2015-12-01 23:30:46 +0300

    Persists user contact data

--- app/src/main/java/kg/gorillagym/gorillagymshop/ContactDetails.java
+++ app/src/main/java/kg/gorillagym/gorillagymshop/ContactDetails.java
@@ -1,6 +1,7 @@
 package kg.gorillagym.gorillagymshop;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +19,11 @@ import ru.egalvi.shop.ClientData;
 
 public class ContactDetails extends AppCompatActivity {
 
+    public static final String EMAIL_FIELD = "email";
+    public static final String NAME_FIELD = "name";
+    public static final String ADDRESS_FIELD = "address";
+    public static final String PHONE_FIELD = "phone";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +40,43 @@ public class ContactDetails extends AppCompatActivity {
         final EditText addressField = (EditText) findViewById(R.id.edit_address);
         final EditText phoneField = (EditText) findViewById(R.id.edit_phone);
 
+        SharedPreferences preferences = getPreferences(0);
+        emailField.setText(preferences.getString(EMAIL_FIELD, ""));
+        nameField.setText(preferences.getString(NAME_FIELD, ""));
+        addressField.setText(preferences.getString(ADDRESS_FIELD, ""));
+        phoneField.setText(preferences.getString(PHONE_FIELD, ""));
+
         Button sendButton = (Button) findViewById(R.id.button_send);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String email = emailField.getText().toString();
+                final String name = nameField.getText().toString();
+                final String address = addressField.getText().toString();
+                final String phone = phoneField.getText().toString();
+                //TODO check fields are not empty
+                //TODO check fields are not equal to previously saved
+                new AlertDialog.Builder(ContactDetails.this)
+                        .setTitle(getString(R.string.save_contact_data))
+                        .setMessage(getString(R.string.really_save_contact_data))
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                SharedPreferences preferences = getPreferences(0);
+                                SharedPreferences.Editor editor = preferences.edit();
+                                editor.putString(EMAIL_FIELD, email);
+                                editor.putString(NAME_FIELD, name);
+                                editor.putString(ADDRESS_FIELD, address);
+                                editor.putString(PHONE_FIELD, phone);
+                                editor.apply();
+                            }
+                        }).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
                 cartService.checkout(CartHolder.getCart(), new ClientData() {
                 });
             }
